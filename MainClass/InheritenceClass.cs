1b3bb95377bb77e607e58ec6eab61d2fb520f628
MaheshSampath123 <83757151+MaheshSampath123@users.noreply.github.com>
2021-05-24 15:37:52 +0530

    changes

--- MainClass/InheritenceClass.cs
+++ MainClass/InheritenceClass.cs
@@ -10,13 +10,23 @@ namespace MainClass
         {
             Console.WriteLine("Method 1");
         }
+        public void test2()
+        {
+            Console.WriteLine("Method 2");
+        }
     }
     class InheritenceClass:Class1
     {
+        public void test3()
+        {
+            Console.WriteLine("Method 3");
+        }
         static void Main(string[] args)
         {
             InheritenceClass n = new InheritenceClass();
             n.test1();
+            n.test2();
+            n.test3();
             Console.ReadLine();
         }
     }

d71488dea467403d75f65ca16da17f9c5055b1a2
MaheshSampath123 <83757151+MaheshSampath123@users.noreply.github.com>
2021-05-24 15:23:35 +0530

    Inher

---
+++ MainClass/InheritenceClass.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MainClass
+{
+    class Class1
+    {
+        public void test1()
+        {
+            Console.WriteLine("Method 1");
+        }
+    }
+    class InheritenceClass:Class1
+    {
+        static void Main(string[] args)
+        {
+            InheritenceClass n = new InheritenceClass();
+            n.test1();
+            Console.ReadLine();
+        }
+    }
+}
---
+++ MainClass/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using anotherone.getting;
+
+namespace MainClass
+{
+    class Program
+    {
+      
+        static void Main(string[] args)
+        {
+            Console.WriteLine("hiiii");
+            Console.ReadLine();
+        }
+    }
+  
+}
